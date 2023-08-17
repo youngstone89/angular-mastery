7ef4aec1191fab08779b53156777231fddcd194f
YeongSeok Kim <yeongseokkim@microsoft.com>
2023-08-17 15:56:57 +0900

    enhanced accordion with cdkDropList and cdkDrag

--- my-first-project/src/app/accordion/accordion.component.ts
+++ my-first-project/src/app/accordion/accordion.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AccordionComponent {
 
+  expressionList = [
+    { title: 'Title 1', summary: 'Summary 1', content: 'Content 1' },
+    { title: 'Title 2', summary: 'Summary 2', content: 'Content 2' },
+    { title: 'Title 3', summary: 'Summary 3', content: 'Content 3' },
+  ]
+  drop($event: any) {
+    console.log($event);
+  }
 }
