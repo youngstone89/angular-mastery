51213ec7522e988281bf1797a44544908e2ed520
YeongSeok Kim <yeongseokkim@microsoft.com>
2023-08-18 13:41:04 +0900

    add button on accordion

--- my-first-project/src/app/accordion/accordion.component.ts
+++ my-first-project/src/app/accordion/accordion.component.ts
@@ -6,12 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./accordion.component.css']
 })
 export class AccordionComponent {
+  expressionList: any[] = [];
+  constructor() {
+    this.expressionList = [
+      { title: 'Title 1', summary: 'Summary 1', content: 'Content 1' },
+      { title: 'Title 2', summary: 'Summary 2', content: 'Content 2' },
+      { title: 'Title 3', summary: 'Summary 3', content: 'Content 3' },
+    ]
+  }
 
-  expressionList = [
-    { title: 'Title 1', summary: 'Summary 1', content: 'Content 1' },
-    { title: 'Title 2', summary: 'Summary 2', content: 'Content 2' },
-    { title: 'Title 3', summary: 'Summary 3', content: 'Content 3' },
-  ]
+  add($event: MouseEvent) {
+    let length = this.expressionList.length;
+    const index = length + 1;
+    this.expressionList.push(
+      { title: 'Title ' + index, summary: 'Summary ' + index, content: 'Content ' + index },
+    )
+  }
+
+
   drop($event: any) {
     console.log($event);
   }
